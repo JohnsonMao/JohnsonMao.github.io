e32621a53a0dfd6d30e401990461c87811e0344f
Johnson Mao <tutelary.maomao@gmail.com>
2023-03-30 21:50:57 +0800

    feature: layout

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link"
+
+function Navbar() {
+  return (
+    <nav className="bg-slate-600 p-4 sticky top-0 drop-shadow-xl z-10">
+      <div className="prose prose-xl mx-auto flex justify-between flex-col sm:flex-row">
+        <Link href="/" className="text-white/90 no-underline hover:text-white">
+          Mao
+        </Link>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
