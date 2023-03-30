466adaa39e2f9f49f8614bdebcf98ab4661bbe6e
Johnson Mao <tutelary.maomao@gmail.com>
2023-03-30 21:50:57 +0800

    feature: layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,26 @@
-export default function Home() {
+import Image from "next/image"
+
+function Home() {
   return (
-    <main>
+    <main className="px-6 mx-auto">
+      <section className="w-full mx-auto">
+        <Image
+          className="border-4 border-black dark:border-slate-500 drop-shadow-xl shadow-black rounded-full mx-auto mt-12"
+          src="/images/mao.jpg"
+          width={200}
+          height={200}
+          alt="Johnson Mao"
+          priority
+        />
+      </section>
+      <p className="my-12 text-3xl text-center dark:text-white">
+        Hello and Welcome &nbsp;
+        <span className="whitespace-nowrap">
+          I&apos;m <span className="font-bold">Johnson</span>.
+        </span>
+      </p>
     </main>
   )
 }
+
+export default Home
