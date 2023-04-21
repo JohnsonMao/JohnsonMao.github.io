2eca5f048cc1a1264e1b440788f7f72273d88966
Johnson Mao <86179381+JohnsonMao@users.noreply.github.com>
2023-04-21 21:28:21 +0800

    Feature/post component (#28)
    * style: add styles to PostList and PostItem
    
    * feature: Add display of categories and tags in post component
    
    * feature: Add image cover in post component

--- components/PostItem.tsx
+++ components/PostItem.tsx
@@ -1,4 +1,8 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import { BsCalendar4Week } from 'react-icons/bs';
+import { MdOutlineWidgets } from 'react-icons/md';
+import { AiOutlineTags } from 'react-icons/ai';
 import getFormattedDate from '@/lib/formatDate';
 
 type PostItemProps = {
@@ -6,19 +10,64 @@ type PostItemProps = {
 };
 
 function PostItem({ post }: PostItemProps) {
-  const { id, title, date, excerpt } = post;
+  const { id, title, date, categories, tags, excerpt, index_img } = post;
   const formattedDate = getFormattedDate(date);
 
   return (
     <li key={id} className="mt-4 text-2xl dark:text-white/90">
-      <Link
-        className="underline hover:text-black/70 dark:hover:text-white/90"
-        href={`/posts/${id}`}
-      >
-        {title}
-      </Link>
-      <p>{excerpt}</p>
-      <p className="mt-1 text-sm">{formattedDate}</p>
+      <article className="flex gap-6">
+        <Image
+          className="shrink-0 object-cover"
+          src={index_img}
+          alt={`${index_img} cover`}
+          width={300}
+          height={200}
+          priority
+        />
+        <div>
+          <h2 className="text-2xl font-bold dark:text-white/90">
+            <Link
+              className="underline hover:text-black/70 dark:hover:text-white/70"
+              href={`/posts/${id}`}
+            >
+              {title}
+            </Link>
+          </h2>
+          <p className="my-3 text-base">{excerpt}</p>
+          <div className="flex flex-wrap gap-x-4 gap-y-1 text-sm">
+            <div className="flex items-center gap-1">
+              <BsCalendar4Week />
+              <time className="whitespace-nowrap">{formattedDate}</time>
+            </div>
+            <div className="flex flex-wrap items-center gap-1">
+              <MdOutlineWidgets />
+              {categories.map((category) =>
+                category.map((tag) => (
+                  <Link
+                    key={category + tag}
+                    className="whitespace-nowrap hover:text-black/70 dark:hover:text-white/70"
+                    href="/"
+                  >
+                    {tag}
+                  </Link>
+                ))
+              )}
+            </div>
+            <div className="flex flex-wrap items-center gap-1">
+              <AiOutlineTags />
+              {tags.map((tag) => (
+                <Link
+                  key={tag}
+                  className="whitespace-nowrap hover:text-black/70 dark:hover:text-white/70"
+                  href="/"
+                >
+                  {tag}
+                </Link>
+              ))}
+            </div>
+          </div>
+        </div>
+      </article>
     </li>
   );
 }
