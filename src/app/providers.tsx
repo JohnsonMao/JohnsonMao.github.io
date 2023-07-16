e5846021b5ebed95ec037acb5a113ed17ae08f9e
Johnson Mao <tutelary.maomao@gmail.com>
2023-07-16 14:26:15 +0800

    ✨ add progress bar feature

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,9 +1,15 @@
 'use client';
 
 import { ThemeProvider } from 'next-themes';
+import { AppProgressBar } from 'next-nprogress-bar';
 
 function Providers({ children }: React.PropsWithChildren) {
-  return <ThemeProvider attribute="class">{children}</ThemeProvider>;
+  return (
+    <ThemeProvider attribute="class">
+      {children}
+      <AppProgressBar height="4px" options={{ showSpinner: false }} />
+    </ThemeProvider>
+  );
 }
 
-export default Providers
+export default Providers;
