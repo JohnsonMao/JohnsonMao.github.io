3732ae39c8ccfc647b1f9044c961425d18767307
Johnson Mao <tutelary.maomao@gmail.com>
2023-07-29 21:48:29 +0800

    ♻️ refactor not found page and middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,29 +1,21 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { defaultLocale, locales } from './i18n';
 
-const X_LANGUAGE_PREFERENCE = 'X-Language-Preference';
-
 export function middleware(request: NextRequest) {
   // Check if there is any supported locale in the pathname
   const pathname = request.nextUrl.pathname;
-  const response = NextResponse.next();
 
   const pathnameIsMissingLocale = locales.every(
     (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
   );
 
-  response.headers.set(X_LANGUAGE_PREFERENCE, 'en');
-
   if (pathnameIsMissingLocale) {
     // e.g. incoming request is /posts
     // Tell Next.js it should pretend it's /{defaultLocale}/posts
     return NextResponse.rewrite(
-      new URL(`/${defaultLocale}${pathname}`, request.url),
-      response
+      new URL(`/${defaultLocale}${pathname}`, request.url)
     );
   }
-
-  return response;
 }
 
 export const config = {
