fee90abb2723a59f13861acf6638cba3eca8df68
Johnson Mao <tutelary.maomao@gmail.com>
2023-09-13 23:45:55 +0800

    ✨ add loading skeleton

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,19 +1,44 @@
+'use client';
+
+import { useState } from 'react';
 import NextImage, { ImageProps as NextImageProps } from 'next/image';
+import cn from '@/utils/cn';
 
 type ImageProps = NextImageProps & { base64?: string };
 
-function Image({ src, height, width, base64, alt, ...otherProps }: ImageProps) {
+function Image({
+  src,
+  height,
+  width,
+  base64,
+  alt,
+  className,
+  onLoadingComplete,
+  ...otherProps
+}: ImageProps) {
+  const [loading, setLoading] = useState(true);
+  const handleComplete = (img: HTMLImageElement) => {
+    setLoading(false);
+    onLoadingComplete?.(img);
+  };
+
   if (!height || !width) {
     return (
-      <picture className="relative block h-96">
+      <picture className={cn('not-prose relative block h-96', className)}>
         <NextImage
           src={src}
           alt={alt}
-          className="m-0 object-contain"
-          sizes="(min-width: 40em) 40em, 100vw"
+          className="object-contain"
+          sizes="100vw"
           fill
+          onLoadingComplete={handleComplete}
           {...otherProps}
         />
+        {loading && (
+          <div className="absolute inset-0 flex items-center justify-center bg-black/30">
+            <div className="h-10 w-10 animate-spin rounded-full border-8 border-white/80 border-t-white/20"></div>
+          </div>
+        )}
       </picture>
     );
   }
@@ -24,9 +49,11 @@ function Image({ src, height, width, base64, alt, ...otherProps }: ImageProps) {
       alt={alt}
       height={height}
       width={width}
-      sizes="(min-width: 40em) 40em, 100vw"
+      sizes="100vw"
       placeholder={base64 ? 'blur' : 'empty'}
       blurDataURL={base64}
+      className={className}
+      onLoadingComplete={handleComplete}
       {...otherProps}
     />
   );
