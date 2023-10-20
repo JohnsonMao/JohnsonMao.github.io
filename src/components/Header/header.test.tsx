3fe9c248555c6ad5a18fb7b8a9b6f95f188d1a19
Johnson Mao <tutelary.maomao@gmail.com>
2023-10-20 16:57:22 +0800

    ✅ update unit test

--- src/components/Header/header.test.tsx
+++ src/components/Header/header.test.tsx
@@ -1,58 +1,32 @@
 import { act, render, screen, waitFor } from '@testing-library/react';
-import Header, { HeaderProps } from '.';
-
-const mockPathname = jest.fn();
-
-jest.mock('next/navigation', () => ({
-  usePathname: () => mockPathname(),
-}));
+import Header from '.';
 
 describe('Header component', () => {
-  const logo = {
+  const avatar = {
     src: 'https://external.com/test.jpg',
-    alt: 'test logo image alt',
+    alt: 'test avatar image alt',
   };
-  const menu: HeaderProps['menu'] = [
-    { text: 'Home', href: '/' },
-    { text: 'Post', href: '/posts' },
-  ];
-
-  beforeEach(() => {
-    mockPathname.mockClear();
-  });
 
   it('should render correct element', () => {
-    mockPathname.mockReturnValueOnce('/');
-
-    render(<Header logo={logo} menu={menu} />);
+    render(<Header avatar={avatar} menu={[]} />);
 
     const nav = screen.getByRole('navigation');
-    const brandLink = screen.getByRole('img', { name: logo.alt });
-    const linkA = screen.getByRole('link', { name: menu[0].text });
-    const linkB = screen.getByRole('link', { name: menu[1].text });
+    const brandLink = screen.getByRole('img', { name: avatar.alt });
 
     expect(nav).toBeInTheDocument();
     expect(nav.tagName).toBe('NAV');
 
     expect(brandLink).toBeInTheDocument();
     expect(brandLink.parentElement).toHaveAttribute('href', '/');
-
-    expect(linkA).toHaveTextContent(menu[0].text);
-    expect(linkA).toHaveAttribute('href', menu[0].href);
-
-    expect(linkB).toHaveTextContent(menu[1].text);
-    expect(linkB).toHaveAttribute('href', menu[1].href);
   });
 
   it('should hide header on scroll down and show on scroll up', async () => {
-    mockPathname.mockReturnValueOnce('/');
-
     Object.defineProperty(HTMLElement.prototype, 'clientHeight', {
       configurable: true,
       value: 50,
     });
 
-    render(<Header logo={logo} menu={[]} />);
+    render(<Header avatar={avatar} menu={[]} scrollThreshold={120} />);
 
     const header = screen.getByRole('banner');
 
@@ -64,7 +38,9 @@ describe('Header component', () => {
     });
 
     await waitFor(() => {
-      expect(header).toHaveStyle({ '--tw-translate-y': '0px' });
+      expect(header).toHaveStyle({ '--header-translate-y': '0px' });
+      expect(header).toHaveStyle({ '--avatar-translate-y': '100.00px' });
+      expect(header).toHaveStyle({ '--avatar-scale': '1.42' });
     });
 
     act(() => {
@@ -73,7 +49,9 @@ describe('Header component', () => {
     });
 
     await waitFor(() => {
-      expect(header).toHaveStyle({ '--tw-translate-y': '-50px' });
+      expect(header).toHaveStyle({ '--header-translate-y': '-50px' });
+      expect(header).toHaveStyle({ '--avatar-translate-y': '0.00px' });
+      expect(header).toHaveStyle({ '--avatar-scale': '1.00' });
     });
 
     act(() => {
@@ -82,7 +60,9 @@ describe('Header component', () => {
     });
 
     await waitFor(() => {
-      expect(header).toHaveStyle({ '--tw-translate-y': '-20px' });
+      expect(header).toHaveStyle({ '--header-translate-y': '-20px' });
+      expect(header).toHaveStyle({ '--avatar-translate-y': '0.00px' });
+      expect(header).toHaveStyle({ '--avatar-scale': '1.00' });
     });
 
     act(() => {
@@ -91,33 +71,44 @@ describe('Header component', () => {
     });
 
     await waitFor(() => {
-      expect(header).toHaveStyle({ '--tw-translate-y': '0px' });
+      expect(header).toHaveStyle({ '--header-translate-y': '0px' });
+      expect(header).toHaveStyle({ '--avatar-translate-y': '40.00px' });
+      expect(header).toHaveStyle({ '--avatar-scale': '1.17' });
+    });
+
+    act(() => {
+      window.scrollY = 0;
+      window.dispatchEvent(new Event('scroll'));
+    });
+
+    await waitFor(() => {
+      expect(header).toHaveStyle({ '--header-translate-y': '0px' });
+      expect(header).toHaveStyle({ '--avatar-translate-y': '120.00px' });
+      expect(header).toHaveStyle({ '--avatar-scale': '1.50' });
     });
   });
 
-  it.each([
-    ['/', menu[0].text],
-    ['/posts', menu[1].text],
-    ['/en', menu[0].text],
-    ['/en/posts', menu[1].text],
-    ['/en/posts/test', menu[1].text],
-  ])(
-    'should render correct active link based on the pathname "%s"',
-    (pathname, activeLinkText) => {
-      mockPathname.mockReturnValueOnce(pathname);
+  it('should calculate the correct styles even when `clientHeight` is `undefined`', async () => {
+    Object.defineProperty(HTMLElement.prototype, 'clientHeight', {
+      configurable: true,
+      value: undefined,
+    });
 
-      render(<Header logo={logo} menu={menu} />);
+    render(<Header avatar={avatar} menu={[]} scrollThreshold={120} />);
 
-      const links = screen.getAllByRole('link');
-      const activeClassName = 'text-blue-500 hover:text-blue-500';
+    const header = screen.getByRole('banner');
 
-      links.forEach((link) => {
-        if (link.textContent === activeLinkText) {
-          expect(link).toHaveClass(activeClassName);
-        } else {
-          expect(link).not.toHaveClass(activeClassName);
-        }
-      });
-    }
-  );
+    expect(header.tagName).toBe('HEADER');
+
+    act(() => {
+      window.scrollY = 200;
+      window.dispatchEvent(new Event('scroll'));
+    });
+
+    await waitFor(() => {
+      expect(header).toHaveStyle({ '--header-translate-y': '0px' });
+      expect(header).toHaveStyle({ '--avatar-translate-y': '0.00px' });
+      expect(header).toHaveStyle({ '--avatar-scale': '1.00' });
+    });
+  });
 });
---
+++ src/components/Header/menu.test.tsx
@@ -0,0 +1,64 @@
+import { render, screen } from '@testing-library/react';
+import Menu, { MenuProps } from './Menu';
+
+const mockPathname = jest.fn();
+
+jest.mock('next/navigation', () => ({
+  usePathname: () => mockPathname(),
+}));
+
+describe('Menu component', () => {
+  const menu: MenuProps['menu'] = [
+    { text: 'Home', href: '/' },
+    { text: 'Post', href: '/posts' },
+  ];
+
+  beforeEach(() => {
+    mockPathname.mockClear();
+  });
+
+  it('should render correct element', () => {
+    mockPathname.mockReturnValueOnce('/');
+
+    render(<Menu menu={menu} />);
+
+    const nav = screen.getByRole('navigation');
+    const linkA = screen.getByRole('link', { name: menu[0].text });
+    const linkB = screen.getByRole('link', { name: menu[1].text });
+
+    expect(nav).toBeInTheDocument();
+    expect(nav.tagName).toBe('NAV');
+
+    expect(linkA).toHaveTextContent(menu[0].text);
+    expect(linkA).toHaveAttribute('href', menu[0].href);
+
+    expect(linkB).toHaveTextContent(menu[1].text);
+    expect(linkB).toHaveAttribute('href', menu[1].href);
+  });
+
+  it.each([
+    ['/', menu[0].text],
+    ['/posts', menu[1].text],
+    ['/en', menu[0].text],
+    ['/en/posts', menu[1].text],
+    ['/en/posts/test', menu[1].text],
+  ])(
+    'should render correct active link based on the pathname "%s"',
+    (pathname, activeLinkText) => {
+      mockPathname.mockReturnValueOnce(pathname);
+
+      render(<Menu menu={menu} />);
+
+      const links = screen.getAllByRole('link');
+      const activeClassName = 'text-blue-500 hover:text-blue-500';
+
+      links.forEach((link) => {
+        if (link.textContent === activeLinkText) {
+          expect(link).toHaveClass(activeClassName);
+        } else {
+          expect(link).not.toHaveClass(activeClassName);
+        }
+      });
+    }
+  );
+});
