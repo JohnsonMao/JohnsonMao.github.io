f298b3f82455c24b103d2714b1d69c93a771bde0
Johnson Mao <tutelary.maomao@gmail.com>
2023-10-21 21:47:38 +0800

    ✅ add pipe and toFixedNumber

--- src/utils/__tests__/math.test.ts
+++ src/utils/__tests__/math.test.ts
@@ -1,13 +1,13 @@
-import { clamp } from '../math';
+import { clamp, pipe, toFixedNumber } from '../math';
 
-describe('clamp function', () => {
-  it.each<[number, number, number, number | undefined]>([
-    [15, 15, 20, undefined],
-    [-5, -5, -20, undefined],
-    [0, -5, 20, undefined],
-    [0, 15, -20, undefined],
-    [20, 50, 20, undefined],
-    [-20, -50, -20, undefined],
+describe('math function', () => {
+  it.each<[number, number, number, number]>([
+    [15, 15, 20, 0],
+    [-5, -5, -20, 0],
+    [0, -5, 20, 0],
+    [0, 15, -20, 0],
+    [20, 50, 20, 0],
+    [-20, -50, -20, 0],
     [-10, 0, -20, -10],
     [-10, 0, -10, -20],
     [10, 0, 10, 20],
@@ -15,8 +15,36 @@ describe('clamp function', () => {
     [5, 5, 20, -20],
   ])(
     '%#. should correctly clamp a value (%o) within the specified range',
-    (expected, ...params) => {
-      expect(clamp(...params)).toBe(expected);
+    (expected, input, ...params) => {
+      expect(clamp(...params)(input)).toBe(expected);
     }
   );
+
+  it('should process an input value through a series of functions in sequence', () => {
+    expect(pipe(1)).toBe(1);
+    expect(pipe(2, (n) => n * 2)).toBe(4);
+    expect(
+      pipe(
+        3,
+        (n) => n * 3,
+        (n) => n + 2
+      )
+    ).toBe(11);
+    expect(
+      pipe(
+        4,
+        (n) => n * 2,
+        (n) => n / 3,
+        (n) => n.toFixed(2)
+      )
+    ).toBe('2.67');
+  });
+
+  it('should round a number to the specified number of decimal places', () => {
+    const input = 1.3456;
+    expect(toFixedNumber(0)(input)).toBe(1);
+    expect(toFixedNumber(1)(input)).toBe(1.3);
+    expect(toFixedNumber(2)(input)).toBe(1.35);
+    expect(toFixedNumber(3)(input)).toBe(1.346);
+  });
 });
