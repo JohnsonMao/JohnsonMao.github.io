dd24638b39785e6579423916ba1a36ed4bd8dcbb
Johnson Mao <tutelary.maomao@gmail.com>
2023-11-03 23:57:49 +0800

    🔥 remove container component forwardRef

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,27 +1,24 @@
-import { HTMLAttributes, forwardRef } from 'react';
+import type { HTMLAttributes } from 'react';
 import cn from '@/utils/cn';
 
 type ContainerProps = {
   as?: 'main' | 'header' | 'footer' | 'div';
 } & HTMLAttributes<HTMLElement>;
 
-const Container = forwardRef<HTMLDivElement, ContainerProps>(
-  function InternalContainer({ as = 'div', className, ...props }, ref) {
-    const Component = as;
+function Container({ as = 'div', className, ...props }: ContainerProps) {
+  const Component = as;
 
-    return (
-      <Component
-        className={cn(
-          'px-7 md:mx-8 lg:mx-16 lg:px-14',
-          as === 'main' &&
-            'mt-4 rounded-lg border-2 border-zinc-500 bg-zinc-100/70 dark:bg-zinc-900/70',
-          className
-        )}
-        ref={ref}
-        {...props}
-      />
-    );
-  }
-);
+  return (
+    <Component
+      className={cn(
+        'px-7 md:mx-8 lg:mx-16 lg:px-14',
+        as === 'main' &&
+          'mt-4 rounded-lg border-2 border-zinc-500 bg-zinc-100/70 dark:bg-zinc-900/70',
+        className
+      )}
+      {...props}
+    />
+  );
+}
 
 export default Container;
