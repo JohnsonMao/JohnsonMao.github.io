31bbd02bda105776f9ac6bc40b129f058036d5e0
Johnson Mao <tutelary.maomao@gmail.com>
2023-12-28 11:31:04 +0800

    ✅ update timer test

--- src/hooks/__tests__/useAutoReset.test.ts
+++ src/hooks/__tests__/useAutoReset.test.ts
@@ -1,4 +1,4 @@
-import { act, renderHook, waitFor } from '@testing-library/react';
+import { act, renderHook } from '@testing-library/react';
 import useAutoReset from '../useAutoReset';
 
 describe('useAutoReset hook', () => {
@@ -7,15 +7,15 @@ describe('useAutoReset hook', () => {
     const initialValue = false;
     const newValue = true;
     const { result } = renderHook(() => useAutoReset(initialValue));
+    jest.useFakeTimers();
     // Assert
     expect(result.current[0]).toBe(initialValue);
     // Act
     act(() => result.current[1](newValue));
     // Assert
     expect(result.current[0]).toBe(newValue);
-    await waitFor(
-      () => expect(result.current[0]).toBe(initialValue),
-      { timeout: 2000 }
-    );
+    act(() => jest.runAllTimers());
+    expect(result.current[0]).toBe(initialValue);
+    jest.useRealTimers();
   });
 });
