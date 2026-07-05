3c5a9b305854cc7b466b8930c4feaf4f9c02ae2f
Johnson Mao <tutelary.maomao@gmail.com>
2026-07-05 15:38:56 +0800

    refactor(pwa): remove push notification support

--- src/env.d.ts
+++ src/env.d.ts
@@ -9,7 +9,6 @@ interface ImportMetaEnv {
   readonly PUBLIC_GISCUS_REPO_ID?: string
   readonly PUBLIC_GISCUS_CATEGORY?: string
   readonly PUBLIC_GISCUS_CATEGORY_ID?: string
-  readonly PUBLIC_VAPID_KEY?: string
 }
 
 interface ImportMeta {
